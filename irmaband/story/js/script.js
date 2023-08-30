2428ed7dc97304b57b2c5edce775554abf5632bc
T2XC <lukas.mettala@gmail.com>
2023-08-30 12:30:17 +0300

    T2XC 2.9.4
    - Story page done

---
+++ irmaband/story/js/script.js
@@ -0,0 +1,9 @@
+/* Sets the width of the side navigation to 250px when opening */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100vw";
+}
+
+/* Sets the width of the side navigation to 0 when closing */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
