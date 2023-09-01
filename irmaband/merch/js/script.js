32af305340636c9973b69b6bbb0f1e23a88161a6
T2XC <lukas.mettala@gmail.com>
2023-09-01 14:49:46 +0300

    T2Xc 2.9.44
    - Shop changes

--- irmaband/merch/js/script.js
+++ irmaband/merch/js/script.js
@@ -7,3 +7,15 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+function pw() {
+    document.querySelector(".productimg").src = "../img/shirtwhite.png";
+}
+
+function pb() {
+    document.querySelector(".productimg").src = "../img/shirtblack.png";
+}
+
+function ps() {
+    document.querySelector(".productimg").src = "../img/shirts.png";
+}
