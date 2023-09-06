442d041eed456d628f3263f3cf6bd21c5eb07f37
T2XC <lukas.mettala@gmail.com>
2023-09-06 13:47:59 +0300

    T2XC 2.9.5
    - Merch fixed
    - News done
    - Main page changed by client request
    - Vuokratraileri fixed

--- irmaband/merch/js/script.js
+++ irmaband/merch/js/script.js
@@ -8,14 +8,98 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-function pw() {
-    document.querySelector(".productimg").src = "../img/shirtwhite.png";
+function pw1() {
+    document.querySelector(".productimg1").src = "../img/shirtwhite.png";
 }
 
-function pb() {
-    document.querySelector(".productimg").src = "../img/shirtblack.png";
+function pb1() {
+    document.querySelector(".productimg1").src = "../img/shirtblack.png";
 }
 
-function ps() {
-    document.querySelector(".productimg").src = "../img/shirts.png";
+function ps1() {
+    document.querySelector(".productimg1").src = "../img/shirts.png";
+}
+
+function pw2() {
+    document.querySelector(".productimg2").src = "../img/shirtwhite.png";
+}
+
+function pb2() {
+    document.querySelector(".productimg2").src = "../img/shirtblack.png";
+}
+
+function ps2() {
+    document.querySelector(".productimg2").src = "../img/shirts.png";
+}
+
+function pw3() {
+    document.querySelector(".productimg3").src = "../img/shirtwhite.png";
+}
+
+function pb3() {
+    document.querySelector(".productimg3").src = "../img/shirtblack.png";
+}
+
+function ps3() {
+    document.querySelector(".productimg3").src = "../img/shirts.png";
+}
+
+function pw4() {
+    document.querySelector(".productimg4").src = "../img/shirtwhite.png";
+}
+
+function pb4() {
+    document.querySelector(".productimg4").src = "../img/shirtblack.png";
+}
+
+function ps4() {
+    document.querySelector(".productimg4").src = "../img/shirts.png";
+}
+
+function pw5() {
+    document.querySelector(".productimg5").src = "../img/shirtwhite.png";
+}
+
+function pb5() {
+    document.querySelector(".productimg5").src = "../img/shirtblack.png";
+}
+
+function ps5() {
+    document.querySelector(".productimg5").src = "../img/shirts.png";
+}
+
+function pw6() {
+    document.querySelector(".productimg6").src = "../img/shirtwhite.png";
+}
+
+function pb6() {
+    document.querySelector(".productimg6").src = "../img/shirtblack.png";
+}
+
+function ps6() {
+    document.querySelector(".productimg6").src = "../img/shirts.png";
+}
+
+function pw7() {
+    document.querySelector(".productimg7").src = "../img/shirtwhite.png";
+}
+
+function pb7() {
+    document.querySelector(".productimg7").src = "../img/shirtblack.png";
+}
+
+function ps7() {
+    document.querySelector(".productimg7").src = "../img/shirts.png";
+}
+
+function pw8() {
+    document.querySelector(".productimg8").src = "../img/shirtwhite.png";
+}
+
+function pb8() {
+    document.querySelector(".productimg8").src = "../img/shirtblack.png";
+}
+
+function ps8() {
+    document.querySelector(".productimg8").src = "../img/shirts.png";
 }
