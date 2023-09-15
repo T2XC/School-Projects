dba16ddbfce5e91e5661d903575dfefdf42aba9e
T2XC <113970818+T2XC@users.noreply.github.com>
2023-09-15 14:11:59 +0300

    Delete acheron directory

--- acheron/js/script.js
+++
@@ -1,9 +0,0 @@
-/* Sets the width of the side navigation to 250px when opening */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-/* Sets the width of the side navigation to 0 when closing */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
